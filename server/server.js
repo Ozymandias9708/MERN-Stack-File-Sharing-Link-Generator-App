6a560dc385196e775c0413337b8427c0d6ee34ed
Shubham Kumar <shubham9905374230@gmail.com>
2023-06-30 17:57:22 +0530

    modifiation in server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,12 @@
 import express from 'express';
 
 
-// dotenv.config();
 
 const app = express();
 import router from './routes/route.js';
 import cors from 'cors';
 import DBConnection from './database/mongoDatabase.js';
 
-// app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use('/', router);
 
 const PORT = 8000;
 app.use(cors());
